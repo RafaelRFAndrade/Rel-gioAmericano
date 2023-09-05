836b750946279796c6c62b534e10087e14165219
RafaelRFAndrade <110788109+RafaelRFAndrade@users.noreply.github.com>
2023-09-05 15:43:05 -0300

    RelógioPronto

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+
 
 namespace RelógioAmericano
 {
@@ -20,9 +23,28 @@ namespace RelógioAmericano
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer;
         public MainWindow()
         {
             InitializeComponent();
+
+            //Meu Timer 
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+
+            //Incio
+            timer.Start();
         }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            string tempoAmericano = now.ToString("hh:mm:ss tt", new System.Globalization.CultureInfo("en-US"));
+
+            horaTextBlock.Text = tempoAmericano.Substring(0, 8);
+            amPmTextBlock.Text = tempoAmericano.Substring(9); //ampm
+        }
+
     }
 }
